454e4c5eff65d03931df3d627a75cbca1edae345
Gormi3815 <gormi3815@wrdsb.ca>
2020-10-04 17:57:13 +0000

    made it find the maxium integer between three numbers

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 /**
  *
  * @author 
@@ -10,6 +11,19 @@ public class Main {
    */
   public static void main(String[] args) {
     // TODO code application logic here
-    
+    Scanner input = new Scanner(System.in);
+    System.out.println("Please enter three integers on seperate lines");
+    int number1 = input.nextInt();
+    int number2 = input.nextInt();
+    int number3 = input.nextInt();
+    if(number1 >= number2 && number1 >= number3){
+      System.out.println("Maximum: " + number1);
+    }
+    else if(number2 >= number1 && number2 >=number3){
+      System.out.println("Maximum" + number2);
+    }
+    else if(number3 >= number1 && number3 >= number2){
+      System.out.println("Maximum" + number3);
+    }
   }
 }
